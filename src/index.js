d61dc2856e84ec28a75f6ddf1394d8456396c8d1
Андрій <rukom@ukr.net>
2024-01-19 01:17:34 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/goit-react-hw-05-movies">
     <React.StrictMode>
       <App />
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </React.StrictMode>
   </BrowserRouter>
 );
