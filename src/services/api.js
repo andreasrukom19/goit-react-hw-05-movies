2f298dd7a1a9079920483fbc85e8a71d8352b46c
Андрій <rukom@ukr.net>
2024-01-19 01:17:22 +0200

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,17 @@ import axios from "axios";
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = 'ec497105dfd303dd7ac9d9da5a0c4cde';
 
-export const fetchTrendListMovies = async () => {
+export const requestTrendListMovies = async () => {
   const { data } = await axios.get(`${BASE_URL}/trending/movie/day?api_key=${API_KEY}`);
   return data.results;
 };
+
+export const requestMovieDetails = async (id) => {
+  const { data } = await axios.get(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
+  return data;
+};
+
+export const requestMovieCredits = async (id) => {
+  const { data } = await axios.get(`${BASE_URL}/movie/${id}/credits?api_key=${API_KEY}`);
+  return data.cast;
+}
