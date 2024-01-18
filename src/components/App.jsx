66d50b8b2293ee13a817e9660a318d090ce8cef8
Андрій <rukom@ukr.net>
2024-01-19 01:17:50 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,20 @@ import Home from 'pages/Home/Home';
 import Movies from 'pages/Movies/Movies';
 import { Route, Routes } from 'react-router-dom';
 import SharedLayout from './SharedLauout/SharedLayout';
+import MovieDetails from 'pages/MovieDetails/MovieDetails';
+import Cast from './Cast/Cast';
+import Reviews from './Reviews/Reviews';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
-        <Route path="/" element={<Home />} />
+        <Route index element={<Home />} />
         <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
       </Route>
     </Routes>
   );
