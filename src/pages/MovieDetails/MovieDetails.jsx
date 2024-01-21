f1cfc6ba5ffc5458b0dd924981391865c4e03009
Андрій <rukom@ukr.net>
2024-01-21 23:45:00 +0200

    Update MovieDetails.jsx

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { Suspense, useEffect, useRef, useState } from 'react';
 import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
 import { requestMovieDetails } from 'services/api';
 import { toast } from 'react-toastify';
 import { GoArrowLeft } from 'react-icons/go';
 import css from './MovieDetails.module.css';
+import img from '../../img/no-photo-icon-22.png';
+import { Loader } from 'components/Loader/Loader';
 
 const MovieDetails = () => {
   const { movieId } = useParams();
@@ -27,23 +29,25 @@ const MovieDetails = () => {
   const releaseYear = releaseDate.getFullYear();
 
   return (
-    <main>
-      <Link to={backLinkRef.current} className={css['back-link']}>
-        <GoArrowLeft />
-        <span>Go back</span>
-      </Link>
+    <main className={css.container}>
+      <div className={css['back-link-container']}>
+        <Link to={backLinkRef.current} className={css['back-link']}>
+          <GoArrowLeft />
+          <span>Go back</span>
+        </Link>
+      </div>
       <section className={css['movie-info-block']}>
         <img
           src={
             movieInfo.poster_path
-              ? `https://image.tmdb.org/t/p/w200${movieInfo.poster_path}`
-              : '🤷'
+              ? `https://image.tmdb.org/t/p/w300${movieInfo.poster_path}`
+              : img
           }
           alt={movieInfo.original_title}
         />
         <div className={css['movie-description']}>
           <h1 className={css['movie-title']}>
-            {movieInfo.original_title} ({releaseYear})
+            {movieInfo.original_title} ({releaseYear ? releaseYear : ''})
           </h1>
           <p className={css['text-block']}>
             User score: {Math.round(movieInfo.vote_average * 10)}%
@@ -56,18 +60,27 @@ const MovieDetails = () => {
           </p>
         </div>
       </section>
-      <section>
-        <p>Additional information</p>
-        <ul>
-          <li>
-            <Link to="cast">Cast</Link>
+      <div className={css.line}></div>
+      <section className={css['additional-info-block']}>
+        <p className={css['additional-info-block-title']}>
+          Additional information
+        </p>
+        <ul className={css['info-link-list']}>
+          <li className={css['info-link-item']}>
+            <Link className={css['info-link']} to="cast">
+              Cast
+            </Link>
           </li>
-          <li>
-            <Link to="reviews">Reviews</Link>
+          <li className={css['info-link-item']}>
+            <Link className={css['info-link']} to="reviews">
+              Reviews
+            </Link>
           </li>
         </ul>
-        <Outlet />
       </section>
+      <Suspense fallback={<Loader />}>
+        <Outlet />
+      </Suspense>
     </main>
   );
 };
